650d1dea507b7d0520096fe4c19eb3c32b798971
Bastardrosset <thexav01@outlook.fr>
2022-12-06 21:28:26 +0100

    Ajout de JWT & bcrypt && modif modelUser complement validator password & ajout de function hash password & compare password && function login

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
+
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -17,13 +19,38 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please provide a password'],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password !']
+        required: [true, 'Please confirm your password !'],
+        validate: {
+            // This only works on create and save
+            validator: function(el) {
+                return el === this.password;
+            }
+        },
+        message: 'Password are not the same'
     }
 });
+
+userSchema.pre('save', async function(next) {
+    // Only run this function if password was actually modified 
+    if (!this.isModified('password')) return next();
+
+    // Hash password
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+    next();
+
+});
+
+// Return true or false
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
